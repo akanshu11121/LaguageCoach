da13b91747f6edd47559a14e2c10990f935ee3af
Akanshu Sonkar <akanshu.sonkar@navanc.com>
2024-08-10 15:31:03 +0530

    initial commit

---
+++ pages/home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+    st.title('Language Coach')
+    st.write('Enhance your language skills with Language Coach, where personalized exercises are tailored just for you!')
